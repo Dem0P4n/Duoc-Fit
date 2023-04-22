779f50af8c2a564d3399fa1f2e3ce1666f7339e3
Dem0P4n <ch.avendano@duocuc.cl>
2023-04-21 20:15:21 -0400

    Commit n°2 "Login 40%"
    1.- Login
    1.1 Diseño del Login 60% Completado Falta:
    -Imagen Acreditacion
    -Boton Olvido contraseña (Diseño Provisional)
    1.2 Funcionalidad del Login 0% Completado Falta:
    -Botones
    -Inicio

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  public formulario!: FormGroup
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+  }
+
+  public submit(){
   }
 
 }
